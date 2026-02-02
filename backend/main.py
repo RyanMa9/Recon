014f114862777bc88b3c69ea12519c14adc4935b
Ryan Ma <ryaenma@gmail.com>
2026-02-02 13:44:29 -0500

    deployment

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,17 @@
 from flask import Flask
 from routes.fighters import fighter_bp
 from flask_cors import CORS
-import os
 
 
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # allow all origins
+    CORS(app)  # allow all origins (development only)
     app.register_blueprint(fighter_bp, url_prefix="/fighters")
     return app
 
+app = create_app()
+
 
 if __name__ == "__main__":
-    app = create_app()
-    port = int(os.environ.get("PORT", 5000))  # default 5000 for local dev
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()
