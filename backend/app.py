995eb334b43bcd1f8ccec3ec1f6171f58526f991
Ryan Ma <ryaenma@gmail.com>
2026-02-02 12:17:05 -0500

    false debug

--- backend/app.py
+++ backend/app.py
@@ -15,4 +15,4 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     port = int(os.environ.get("PORT", 5000))  # default 5000 for local dev
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
