a867c158a20547962605c581b3ebe3d5cc242e70
Ryan Ma <ryaenma@gmail.com>
2026-03-31 13:45:12 -0400

    env changes

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from routes.fighters import fighter_bp
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # allow all origins (development only)
+    CORS(app, origins=[FRONTEND_URL])  
     app.register_blueprint(fighter_bp, url_prefix="/fighters")
     return app
 
